64cc1173d68bdf960d52a17796a850ecf8da872a
gsemir0418 <845217811@qq.com>
2022-05-19 17:42:09 +0800

    配置tsx支持

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+  const count = ref(0)
+  const onClick = () => {
+    count.value += 1
+  }
+</script>
+
 <template>
-<h1>
-  hi
-</h1>
+  <h1>
+    {{count}}
+  </h1>
+  <div>
+    <button @click="onClick">+1</button>
+  </div>
 </template>
+
+<style>
+</style>
