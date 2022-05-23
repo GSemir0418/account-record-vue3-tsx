ee7ede65707fa0c2f56cae561ce9eecce8b6291e
gsemir0418 <845217811@qq.com>
2022-05-23 14:25:54 +0800

    完成CSS初始化 sass支持

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import { defineComponent } from "vue";
-import { RouterLink, RouterView } from "vue-router";
-
+import { RouterView } from "vue-router";
+import "./App.scss";
 export const App = defineComponent({
   setup() {
     return () => (
       <>
-        <header>导航</header>
-        <div>
-          <ul>
-            <li>
-              {/* 路由跳转链接 */}
-              <RouterLink to={"/"}>Foo</RouterLink>
-            </li>
-            <li>
-              <RouterLink to={"/bar"}>Bar</RouterLink>
-            </li>
-          </ul>
-        </div>
         {/* 展示路由视图的区域 */}
         <RouterView />
-        <footer>页脚</footer>
       </>
     );
   },
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,11 +1,20 @@
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
+import s from "./Welcome.module.scss";
+import mangosteen from "../assets/icons/mangosteen.svg";
 export const Welcome = defineComponent({
   setup(props, context) {
     return () => (
-      <div>
+      <div class={s.wrapper}>
+        <header>
+          <img src={mangosteen} alt="" />
+          <h1>GS记账</h1>
+        </header>
         {/* 需要定义子路由渲染组件 */}
-        <RouterView />
+        <main>
+          <RouterView />
+        </main>
+        <footer>buttons</footer>
       </div>
     );
   },
