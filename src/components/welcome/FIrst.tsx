db487d7c425906152e094aed6af9eb069ba61c87
gsemir0418 <845217811@qq.com>
2022-05-23 16:59:57 +0800

    完成第一个欢迎界面

--- src/components/welcome/FIrst.tsx
+++ src/components/welcome/FIrst.tsx
@@ -1,6 +1,31 @@
 import { defineComponent } from "vue";
+import s from "./First.module.scss";
+import pig from "../../assets/icons/pig.svg";
+import { RouterLink } from "vue-router";
 export const First = defineComponent({
   setup(props, context) {
-    return () => <div>First</div>;
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img src={pig} alt="" />
+          <h2>
+            会赚钱
+            <br />
+            还要会省钱
+          </h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink to="/welcome/4" class={s.fake}>
+            跳过
+          </RouterLink>
+          <RouterLink to="/welcome/2" class={s.next}>
+            下一页
+          </RouterLink>
+          <RouterLink to="/welcome/4" class={s.skip}>
+            跳过
+          </RouterLink>
+        </div>
+      </div>
+    );
   },
 });
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -14,7 +14,6 @@ export const Welcome = defineComponent({
         <main>
           <RouterView />
         </main>
-        <footer>buttons</footer>
       </div>
     );
   },
