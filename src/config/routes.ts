6726b63f2d154de04060f90fdd202057f6a46ba0
gsemir0418 <845217811@qq.com>
2022-05-23 19:51:07 +0800

    解决移动端Edge浏览器不能全屏的bug

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -3,18 +3,16 @@ import { First } from "../components/welcome/FIrst";
 import { Forth } from "../components/welcome/Forth";
 import { Second } from "../components/welcome/Second";
 import { Third } from "../components/welcome/Third";
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
-  { path: "/", component: Foo },
-  { path: "/bar", component: Bar },
+  { path: "/", redirect: "/welcome" },
   {
     path: "/welcome",
     component: Welcome,
     children: [
       // 注意path不需要'/'
+      { path: "", redirect: "/welcome/1" },
       { path: "1", component: First },
       { path: "2", component: Second },
       { path: "3", component: Third },
