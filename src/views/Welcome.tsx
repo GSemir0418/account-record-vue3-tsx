5b36c03c95f53a8abcdc90288e956a79ad032790
gsemir0418 <845217811@qq.com>
2022-05-25 11:06:22 +0800

    添加Transition过渡

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,7 +1,8 @@
-import { defineComponent } from "vue";
-import { RouterView } from "vue-router";
+import { defineComponent, Transition, VNode } from "vue";
+import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
 import s from "./Welcome.module.scss";
 import mangosteen from "../assets/icons/mangosteen.svg";
+
 export const Welcome = defineComponent({
   setup(props, context) {
     return () => (
@@ -11,7 +12,24 @@ export const Welcome = defineComponent({
           <h1>GS记账</h1>
         </header>
         <main>
-          <RouterView name="main" />
+          <RouterView name="main">
+            {({
+              Component,
+              route,
+            }: {
+              Component: VNode;
+              route: RouteLocationNormalizedLoaded;
+            }) => (
+              <Transition
+                enterFromClass={s.slide_fade_enter_from}
+                enterActiveClass={s.slide_fade_enter_active}
+                leaveToClass={s.slide_fade_leave_to}
+                leaveActiveClass={s.slide_fade_leave_active}
+              >
+                {Component}
+              </Transition>
+            )}
+          </RouterView>
         </main>
         <footer>
           <RouterView name="footer" />
