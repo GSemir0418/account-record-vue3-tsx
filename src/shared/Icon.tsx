5493ad74b4164eab22a41312437904047c9cf081
gsemir0418 <845217811@qq.com>
2022-05-26 15:33:58 +0800

    封装Icon组件

---
+++ src/shared/Icon.tsx
@@ -0,0 +1,20 @@
+import { defineComponent, PropType } from "vue";
+import s from "./Icon.module.scss";
+export const Icon = defineComponent({
+  props: {
+    name: {
+      // 前面的类型是为Vue准备的，后面的是为TypeScript准备的
+      type: String as PropType<
+        "add" | "chart" | "clock" | "cloud" | "pig" | "mangosteen"
+      >,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    return () => (
+      <svg class={s.icon}>
+        <use xlinkHref={`#${props.name}`}></use>
+      </svg>
+    );
+  },
+});
