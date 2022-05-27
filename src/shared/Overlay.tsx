04eddd68b206a877e0a572405ea0beb0b3db9e4a
gsemir0418 <845217811@qq.com>
2022-05-27 14:47:05 +0800

    Overlay组件样式

--- src/shared/Overlay.tsx
+++ src/shared/Overlay.tsx
@@ -1,6 +1,9 @@
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, ref, watchEffect } from "vue";
+import { RouterLink } from "vue-router";
+import { useSwipe } from "../hooks/useSwipe";
 import { Icon } from "./Icon";
 import s from "./Overlay.module.scss";
+import { throttle } from "./throttle";
 export const Overlay = defineComponent({
   props: {
     close: {
@@ -9,30 +12,55 @@ export const Overlay = defineComponent({
     },
   },
   setup(props, context) {
+    // 滑动关闭
+    const overlayRef = ref();
+    const { distance, direction, isMoving } = useSwipe(overlayRef, {
+      beforeStart: (e) => e.preventDefault(),
+    });
+    const handleSwipe = throttle(() => {
+      props.close();
+    }, 500);
+    watchEffect(() => {
+      if (
+        direction.value === "left" &&
+        isMoving.value &&
+        distance.value?.x &&
+        Math.abs(distance.value.x) > 100
+      ) {
+        handleSwipe();
+      }
+    });
     const close = () => {
       props.close();
     };
+    const onClickSignIn = () => {};
     return () => (
       <>
         <div class={s.mask} onClick={close}></div>
-        <div class={s.overlay}>
-          <section>
-            <p>未登录用户</p>
+        <div class={s.overlay} ref={overlayRef}>
+          <section class={s.currentUser} onClick={onClickSignIn}>
+            <h2>未登录用户</h2>
             <p>点击这里登录</p>
           </section>
           <nav>
-            <ul>
+            <ul class={s.action_list}>
               <li>
-                <Icon name="charts" />
-                <span>统计图表</span>
+                <RouterLink to="/statistics" class={s.action}>
+                  <Icon name="charts" class={s.icon} />
+                  <span>统计图表</span>
+                </RouterLink>
               </li>
               <li>
-                <Icon name="export" />
-                <span>导出数据</span>
+                <RouterLink to="/export" class={s.action}>
+                  <Icon name="export" class={s.icon} />
+                  <span>导出数据</span>
+                </RouterLink>
               </li>
               <li>
-                <Icon name="notify" />
-                <span>记账提醒</span>
+                <RouterLink to="/notify" class={s.action}>
+                  <Icon name="notify" class={s.icon} />
+                  <span>记账提醒</span>
+                </RouterLink>
               </li>
             </ul>
           </nav>
