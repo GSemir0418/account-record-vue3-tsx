de5627d791a198b2e464ef1689e7873475fb13ed
gsemir0418 <845217811@qq.com>
2022-05-30 17:09:34 +0800

    完成数字按键功能

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -15,20 +15,114 @@ export const InputPad = defineComponent({
       refDate.value = date;
       onCancel();
     };
-
+    // 数字输入功能
+    const refAmount = ref("0");
+    const appendText = (n: number | string) => {
+      const nString = n.toString();
+      const dotIndex = refAmount.value.indexOf(".");
+      // 位数限制，最大13位
+      if (refAmount.value.length >= 13) {
+        return;
+      }
+      // 位数限制，小数点后最多两位
+      if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
+        return;
+      }
+      // 如果输入的是小数点
+      if (nString === ".") {
+        // 如果已经存在小数点，则不能再次输入
+        if (dotIndex >= 0) {
+          return;
+        }
+        // 如果输入的是0
+      } else if (nString === "0") {
+        // 当第一次输入时（一开始就输入0），则不生效
+        if (refAmount.value === "0") {
+          return;
+        }
+        // 如果输入的是其他（0-9）
+      } else {
+        // 把默认的0删掉
+        if (refAmount.value === "0") {
+          refAmount.value = "";
+        }
+      }
+      // 赋值
+      refAmount.value += n.toString();
+    };
     const buttons = [
-      { text: "1", onClick: () => {} },
-      { text: "2", onClick: () => {} },
-      { text: "3", onClick: () => {} },
-      { text: "4", onClick: () => {} },
-      { text: "5", onClick: () => {} },
-      { text: "6", onClick: () => {} },
-      { text: "7", onClick: () => {} },
-      { text: "8", onClick: () => {} },
-      { text: "9", onClick: () => {} },
-      { text: ".", onClick: () => {} },
-      { text: "0", onClick: () => {} },
-      { text: "清空", onClick: () => {} },
+      {
+        text: "1",
+        onClick: () => {
+          appendText("1");
+        },
+      },
+      {
+        text: "2",
+        onClick: () => {
+          appendText("2");
+        },
+      },
+      {
+        text: "3",
+        onClick: () => {
+          appendText("3");
+        },
+      },
+      {
+        text: "4",
+        onClick: () => {
+          appendText("4");
+        },
+      },
+      {
+        text: "5",
+        onClick: () => {
+          appendText("5");
+        },
+      },
+      {
+        text: "6",
+        onClick: () => {
+          appendText("6");
+        },
+      },
+      {
+        text: "7",
+        onClick: () => {
+          appendText("7");
+        },
+      },
+      {
+        text: "8",
+        onClick: () => {
+          appendText("8");
+        },
+      },
+      {
+        text: "9",
+        onClick: () => {
+          appendText("9");
+        },
+      },
+      {
+        text: ".",
+        onClick: () => {
+          appendText(".");
+        },
+      },
+      {
+        text: "0",
+        onClick: () => {
+          appendText("0");
+        },
+      },
+      {
+        text: "清空",
+        onClick: () => {
+          refAmount.value = "0";
+        },
+      },
       { text: "提交", onClick: () => {} },
     ];
     return () => (
@@ -51,7 +145,7 @@ export const InputPad = defineComponent({
               </Popup>
             </span>
           </span>
-          <span class={s.amount}>199.00</span>
+          <span class={s.amount}>{refAmount.value}</span>
         </div>
         <div class={s.buttons}>
           {buttons.map((button) => (
