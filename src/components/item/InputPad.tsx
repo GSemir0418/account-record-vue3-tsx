096d4b148785a529f44906b02db1e4e580f2c205
gsemir0418 <845217811@qq.com>
2022-05-30 16:22:02 +0800

    利用Vant完成日期选择器

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,8 +1,21 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { Icon } from "../../shared/Icon";
+import { DatetimePicker, Popup } from "vant";
+import { time } from "../../shared/time";
 import s from "./InputPad.module.scss";
 export const InputPad = defineComponent({
   setup(props, context) {
+    // 日期选择器功能
+    const now = new Date();
+    const refDate = ref<Date>(now);
+    const refIsDatePickerShow = ref(false);
+    const showDatePicker = () => (refIsDatePickerShow.value = true);
+    const onCancel = () => (refIsDatePickerShow.value = false);
+    const onConfirm = (date: Date) => {
+      refDate.value = date;
+      onCancel();
+    };
+
     const buttons = [
       { text: "1", onClick: () => {} },
       { text: "2", onClick: () => {} },
@@ -23,7 +36,20 @@ export const InputPad = defineComponent({
         <div class={s.dateAndAmount}>
           <span class={s.date}>
             <Icon name="date" class={s.icon}></Icon>
-            <span>2020-09-09</span>
+            <span>
+              <span onClick={showDatePicker}>
+                {time(refDate.value).format()}
+              </span>
+              <Popup position="bottom" v-model:show={refIsDatePickerShow.value}>
+                <DatetimePicker
+                  value={refDate.value}
+                  type="date"
+                  title="选择年月日"
+                  onConfirm={onConfirm}
+                  onCancel={onCancel}
+                />
+              </Popup>
+            </span>
           </span>
           <span class={s.amount}>199.00</span>
         </div>
