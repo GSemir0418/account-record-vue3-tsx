38c896f0b5eb9d3d7966b88de5fd37c229c426f8
gsemir0418 <845217811@qq.com>
2022-06-01 10:45:39 +0800

    设计表单验证器

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, reactive } from "vue";
+import { defineComponent, reactive, toRaw } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Button } from "../../shared/Button";
 import { EmojiList } from "../../shared/EmojiList";
@@ -10,13 +10,30 @@ export const TagCreate = defineComponent({
       name: "",
       sign: "",
     });
+    const onSubmit = (e: Event) => {
+      // 取消默认行为(提交后会刷新)
+      e.preventDefault();
+      // 拿到formData原始对象(原来是proxy对象)
+      const data = toRaw(formData);
+      // 设置校验规则
+      const rules = [
+        { key: "name", required: true, message: "请输入标签名称" },
+        { key: "name", pattern: /^.{2,6}$/, message: "标签名称长度为2-6" },
+        { key: "sign", required: true, message: "请输入标签签名" },
+      ];
+      // const errors = validate(data, rules);
+      // errors = {
+      //   name: ["错误1", "错误2"],
+      //   sign: ["错误3"],
+      // };
+    };
     return () => (
       <MainLayout>
         {{
           default: () => "新建标签",
           icon: () => <Icon name="left" />,
           main: () => (
-            <form class={s.form}>
+            <form class={s.form} onSubmit={onSubmit}>
               {/* 标签名称表单项 */}
               <div class={s.formRow}>
                 <label class={s.formLabel}>
