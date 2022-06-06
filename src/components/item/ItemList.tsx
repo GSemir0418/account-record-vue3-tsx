d65547ce65d3fb74f5eb211ea658c086834e0638
gsemir0418 <845217811@qq.com>
2022-06-06 17:35:00 +0800

    扩展了Tabs组件，支持classPrefix，解决选择器优先级导致的样式重写bug

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,7 +1,33 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import { MainLayout } from "../../layouts/MainLayout";
+import { Icon } from "../../shared/Icon";
+import { Tab, Tabs } from "../../shared/Tabs";
 import s from "./ItemList.module.scss";
 export const ItemList = defineComponent({
   setup(props, context) {
-    return () => <div>ItemList</div>;
+    const refSelected = ref("本月");
+    return () => (
+      <MainLayout>
+        {{
+          default: () => "账单列表",
+          icon: () => (
+            <Icon name="menu" class={s.icon}>
+              列表
+            </Icon>
+          ),
+          main: () => (
+            <Tabs
+              classPrefix={"customTabs"}
+              v-model:selected={refSelected.value}
+            >
+              <Tab name="本月">list1</Tab>
+              <Tab name="上月">list2</Tab>
+              <Tab name="今年">list3</Tab>
+              <Tab name="自定义时间">list4</Tab>
+            </Tabs>
+          ),
+        }}
+      </MainLayout>
+    );
   },
 });
--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -2,6 +2,9 @@ import { defineComponent, PropType } from "vue";
 import s from "./Tabs.module.scss";
 export const Tabs = defineComponent({
   props: {
+    classPrefix: {
+      type: String,
+    },
     selected: {
       type: String as PropType<string>,
     },
@@ -19,12 +22,18 @@ export const Tabs = defineComponent({
           throw new Error("Tabs组件的子组件必须是Tab组件");
         }
       }
+      const cp = props.classPrefix;
       return (
-        <div class={s.tabs}>
-          <ol class={s.tabs_nav}>
+        <div class={[s.tabs, cp + "_tabs"]}>
+          <ol class={[s.tabs_nav, cp + "_tabs_nav"]}>
             {nodeArr.map((item) => (
               <li
-                class={item.props?.name === props.selected ? s.selected : ""}
+                class={[
+                  item.props?.name === props.selected
+                    ? [s.selected, cp + "_selected"]
+                    : "",
+                  cp + "_tabs_nav_item",
+                ]}
                 onClick={() =>
                   context.emit("update:selected", item.props?.name)
                 }
@@ -33,7 +42,9 @@ export const Tabs = defineComponent({
               </li>
             ))}
           </ol>
-          <div>{nodeArr.find((item) => item.props?.name === props.selected)}</div>
+          <div>
+            {nodeArr.find((item) => item.props?.name === props.selected)}
+          </div>
         </div>
       );
     };
